f2dc02c9381bb7c03629754d065027741db521a0
errorr404 <dkbishwash@gmail.com>
2018-04-05 22:50:59 +0530

    Adding the new data structure for maintain expenses and Income and a unique ID for every event

--- app.js
+++ app.js
@@ -28,6 +28,40 @@ var budgetController = (function(){
         }
     };
     
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+            
+            // Create new ID
+            if(data.allItems[type].length>0) {
+                  ID = data.allItems[type][data.allItems[type].length - 1 ].id + 1;
+            }
+            else {
+                ID=0;
+            }
+          
+            
+            // Create new item based on 'inc' or 'exp' type
+            
+            if(type==='exp'){
+                newItem = new Expense(ID,des,val);
+            }
+            else if(type==='inc'){
+                newItem= new Income(ID,des,val);
+            }
+            
+            //  push it into our data structutre
+            data.allItems[type].push(newItem);
+            
+            // Return the new element
+            return newItem;
+            
+        },
+        testing: function(){
+            console.log(data);
+        }
+    };
+    
     
     
 })();
@@ -84,12 +118,15 @@ var controller = (function(budgetctrl, UICtrl){
  
      
     var ctrlAddItem = function(){
+        var input,newItem;
          // 1.get the field input data
        
-        var input = UICtrl.getInput();
+         input = UICtrl.getInput();
       
        // 2. Add the item to the budget controller. 
        
+       newItem =  budgetctrl.addItem(input.type, input.description, input.value);
+        
        // 3. Add the item to the UI
        
        // 4. Calculate the budget
